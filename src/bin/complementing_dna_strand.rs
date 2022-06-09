48f7a74f3a50bfb150078abe5113a87101e67f96
Curro Campuzano Jiménez <campuzanocurro@gmail.com>
2022-06-09 15:39:52 +0200

    feat: complementing strand

---
+++ src/bin/complementing_dna_strand.rs
@@ -0,0 +1,16 @@
+// Complementing a Strand of DNA
+// Given: A DNA string s of length at most 1000 bp.
+// Return: The reverse complement sc of s.
+use std::io;
+use rosalind_rust::dealing_with_nucleotides;
+fn main() {
+    println!("Complementing a DNA strand 🧬");
+    let mut sequence = String::new();
+    io::stdin()
+        .read_line(&mut sequence)
+        .expect("Failed to read line");
+    let result = dealing_with_nucleotides::get_complementing_dna_strand(&sequence.
+        trim()
+        .to_uppercase());
+    println!("{}",result)
+}
