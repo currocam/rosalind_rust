93da778d91901f0923809df984dd70322db96811
Curro Campuzano Jiménez <campuzanocurro@gmail.com>
2022-06-11 17:42:49 +0200

    refactor: idiomatic test str

--- src/dealing_with_nucleotides/tests.rs
+++
@@ -1,8 +0,0 @@
-use crate::dealing_with_nucleotides::transcribing_dna_into_rna;
-#[test]
-fn it_works_when_transcribing_dna_into_rna() {
-    let dna_string = String::from("GATGGAACTTGACTACGTAAATT");
-    let rna_string = String::from("GAUGGAACUUGACUACGUAAAUU");
-    let result = transcribing_dna_into_rna(&dna_string);
-    assert_eq!(result, rna_string);
-}
